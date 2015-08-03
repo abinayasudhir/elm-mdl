66e8b1ebe6193ea619c640b9f8b1e15d6d5f5ca8
Peter Damoc <pdamoc@gmail.com>
2015-08-03 18:29:25 +0300

    updated the files to pass the elm-make checks

--- src/MDL.elm
+++ src/MDL.elm
@@ -1,7 +1,20 @@
 module MDL where 
 
-import Html exposing (node, text, button, i)
+{-| This library provides shortcuts for Material Design Lite library.
+
+# Definition
+@docs icon
+
+
+-}
+
+import Html exposing (Html, node, text, button, i)
 import Html.Attributes exposing (class, disabled, style)
 
 
+{-| material-icons shortcut.
+
+    icon "add"
+-}
+icon : String -> Html
 icon name = i [class "material-icons"] [text name]
