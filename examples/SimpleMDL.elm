fe4dfe0be449259694d95487119f11c6e320b916
Peter Damoc <pdamoc@gmail.com>
2015-08-04 16:10:57 +0300

    simple button example

---
+++ examples/SimpleMDL.elm
@@ -0,0 +1,12 @@
+import MDL exposing (..)
+import Html exposing (div, button, text, br)
+import Html.Attributes exposing (..)
+
+
+main = page <| div [] 
+    [ button [btnClsShorthand "icon accent ripple"]  [icon "mood"]
+    , br [] []
+    , button [btnClsShorthand "raised accent ripple"]  [text "moodovich"]
+    , br [] []
+    , button [class "mdl-button mdl-js-button", disabled True]  [text "boda"]
+    ]
